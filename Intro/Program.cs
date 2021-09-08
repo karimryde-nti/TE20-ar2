85cfa2ec1fa7c2a1b62a5d089d82db066d8015cb
karye <karye.webb@gmail.com>
2021-09-08 11:03:28 +0200

    Tränat på cw och specialtecken

---
+++ Intro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mitt namn är Karim Ryde");
+            Console.WriteLine("It is a long established fact that a reader\n" + "will be distracted by the readable content of a page when\n" + "looking! at its layout. ");
+
+            Console.WriteLine("I prog 1 läser vi C#");
+
+            Console.WriteLine("Mitt spelnick är \"Von Clomp\"");
+        }
+    }
+}
