1536d7a6ae2db2e55871a25c520dac85e3275fe7
Karim Ryde <karye.webb@gmail.com>
2021-10-05 11:01:54 +0200

    Tränat på loopar

---
+++ Kapitel-4/GissaETtTal2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GissaETtTal2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Spelet Gissa ett tal!!");
+
+            // Slumpa fram ett tal
+            Random tärning = new Random();
+            int slumptal = tärning.Next(1, 51);
+
+            // Loopa 5 gånger
+            int räknare = 0;
+            while (true)
+            {
+                // Räkna upp varv
+                räknare++;
+
+                // Bryt efter 5 varv
+                if (räknare >= 5)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Kapitel-4/GissaEttTal/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace GissaEttTal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Spel - gissa ett tal mellan 1 och 100.");
+
+            // Spela flera gånger
+            while (true)
+            {
+                // Slumpa fram ett tal
+                Random tärning = new Random();
+                int slumptal = tärning.Next(1, 101);
+
+                // Upprepa frågan
+                int räknare = 0;
+                while (true)
+                {
+                    // Räkna upp antal gissningar = varv
+                    räknare++;
+
+                    // Fråga användaren om en gissning
+                    Console.Write("Gissa ett tal (1-100): ");
+                    int gissning = int.Parse(Console.ReadLine());
+
+                    // Är gissningen rätt?
+                    if (gissning == slumptal)
+                    {
+                        Console.WriteLine($"Bra gissat! Du gjorde på {räknare} försök");
+                        break;
+                    }
+
+                    // Jämför med slumptal
+                    if (gissning < slumptal)
+                    {
+                        Console.WriteLine("För lågt!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("För högt!");
+                    }
+                }
+                Console.WriteLine("Nu får du göra ett till försök!");
+            }
+        }
+    }
+}
