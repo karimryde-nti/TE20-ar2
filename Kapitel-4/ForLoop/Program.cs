4940a8152fc9d54af9b4c8f5a14d7bfa6a15d0dd
Karim Ryde <karye.webb@gmail.com>
2021-10-19 12:01:07 +0200

    Intro till char

---
+++ Kapitel-4/ForLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Räkna 1 till 10");
+
+            // for (int i = 1; i < 11; i++)
+            // {
+            //     Console.WriteLine($"Loop nr {i}");
+            // }
+
+            // Läs in en meddelande
+            Console.WriteLine("Skriv ett meddelande");
+            string meddelande = Console.ReadLine();
+            
+            // Loopa igenom meddelandet
+            for (int i = 0; i < meddelande.Length; i++)
+            {
+                Console.WriteLine(meddelande[i]);
+            }
+        }
+    }
+}
