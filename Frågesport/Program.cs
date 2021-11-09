1f6ad17ba10a97ac63ba2590575ff9ca55c9946a
Karim Ryde <karye.webb@gmail.com>
2021-11-09 11:11:12 +0100

    Början på projekt frågesport

---
+++ Frågesport/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Frågesport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Frågesport!");
+
+            // Första fråga
+            Console.WriteLine("Vilken 1960-tal amerikanska popgrupp skapade 'surfin-ljudet'?");
+            Console.WriteLine("a - The Beach Boys");
+            Console.WriteLine("b - The Monkeys");
+            Console.WriteLine("c - The Who");
+            string svar1 = Console.ReadLine().ToLower(); // Bara läser in små bokstäver
+            
+            // Kolla om svaret är rätt
+            if (svar1 == "a")
+            {
+                Console.WriteLine("Rätt svar");
+            }
+            else
+            {
+                Console.WriteLine("Fel svar");
+            }
+        }
+    }
+}
