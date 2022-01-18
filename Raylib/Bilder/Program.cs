bdec652d8b8bd8ebff78f14f70dc906d292bedfc
Karye <karye@users.noreply.github.com>
2022-01-18 11:12:11 +0100

    Träna på bilder & input

---
+++ Raylib/Bilder/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Bilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Starta grafikmotorn
+            Raylib.InitWindow(800, 600, "Mitt Raylib fönster");
+
+            // Bestäm skärmuppdatering
+            Raylib.SetTargetFPS(60);
+
+            // Ladda in bilden
+            Texture2D månen = Raylib.LoadTexture(@"./resurser/moon.png");
+
+            // Animationsloopen
+            while (!Raylib.WindowShouldClose())
+            {
+                // Börja rita
+                Raylib.BeginDrawing();
+
+                // Töm ritytan
+                Raylib.ClearBackground(Color.DARKBLUE);
+
+                // Rita ut en bild
+                Raylib.DrawTexture(månen, 100, 100, Color.WHITE);
+
+                // Ritat ut på fönstret
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
---
+++ Raylib/Input/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Bilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Starta grafikmotorn
+            Raylib.InitWindow(800, 600, "Mitt Raylib fönster");
+
+            // Bestäm skärmuppdatering
+            Raylib.SetTargetFPS(60);
+
+            // Skapa en spelare av en rektangel
+            Rectangle spelare = new Rectangle(100, 100, 50, 50);
+            // Ladda bild för spelaren
+            Texture2D raket = Raylib.LoadTexture(@"./resurser/raket.png");
+
+            // Animationsloopen
+            while (!Raylib.WindowShouldClose())
+            {
+                /* Rita ut grafiken */
+                // Börja rita
+                Raylib.BeginDrawing();
+
+                // Töm ritytan
+                Raylib.ClearBackground(Color.DARKBLUE);
+
+                // Rita ut spelarens bild istället för rektangeln
+                Raylib.DrawTexture(raket, (int)spelare.x, (int)spelare.y, Color.WHITE);
+            
+                // Ritat ut på fönstret
+                Raylib.EndDrawing();
+
+                /* Interaktion med användaren */
+                // Lyssna på tangenter
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+                {
+                    spelare.x += 2;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+                {
+                    spelare.x -= 2;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+                {
+                    spelare.y += 2;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+                {
+                    spelare.y -= 2;
+                }
+            }
+        }
+    }
+}
