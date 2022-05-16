0cb01ba7450162663bd9728c2467e59066f0b5ee
Karye <karye@users.noreply.github.com>
2022-05-16 16:33:21 +0200

    Tränat inför prov

---
+++ FLF-160522/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace FLF_160522
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Metoder!");
+
+            /* // Anropa metoden
+            VertikalUtskrift("Karim Ryde");
+
+            Console.Write("Skriv in ditt namn: ");
+            string namn2 = Console.ReadLine();
+            VertikalUtskrift(namn2); */
+
+            // Använd metod 2
+            int tal1 = SlumpaTal();
+            int tal2 = SlumpaTal();
+            Console.WriteLine($"Summan är {tal1 + tal2}");
+
+            SkrivUtKvadrat(3);
+            SkrivUtKvadrat(4);
+
+            RepeteraOrd("Yara");
+        }
+
+        // En metod
+        static void VertikalUtskrift(string namn)
+        {
+            for (int i = 0; i < namn.Length; i++)
+            {
+                Console.WriteLine(namn[i]);
+            }
+        }
+
+        // En metod föra att slumpa fram ett tal 1-100
+        static int SlumpaTal()
+        {
+            Random generator = new Random();
+            int slumptal = generator.Next(0, 101);
+            //Console.WriteLine($"Vi har ett slumptal {slumptal}");
+            return slumptal;
+        }
+
+        // En metod som skriver ut kvadraten av talet man stoppar in
+        static void SkrivUtKvadrat(int tal)
+        {
+            Console.WriteLine(tal * tal);
+        }
+
+        // En metod som skriver ut ett ord 5 gånger
+        static void RepeteraOrd(string ord)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(ord);
+            }
+        }
+    }
+}
---
+++ TryParse/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ange ett tal");
+            //int tal = int.Parse(Console.ReadLine());
+
+            int heltal;
+            // Lyckas vi läsa in ett tal
+            bool funkar = int.TryParse(Console.ReadLine(), out heltal);
+
+            // Gick det?
+            if (funkar == true)
+            {
+                Console.WriteLine($"Du matade in heltalet {heltal}");
+            }
+            else
+            {
+                Console.WriteLine("Du matade inte i ett tal");
+            }
+        }
+
+        // En metod för att läsa in ett tal
+        static int ReadInt()
+        {
+            Console.Write("Ange ett tal");
+            int heltal;
+
+            while (true)
+            {
+                // Lyckas vi läsa in ett tal
+                bool funkar = int.TryParse(Console.ReadLine(), out heltal);
+
+                // Gick det?
+                if (funkar == true)
+                {
+                    Console.WriteLine($"Du matade in heltalet {heltal}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Du matade inte i ett tal, vg försök igen ");
+                }
+            }
+
+            return heltal;
+        }
+    }
+}
